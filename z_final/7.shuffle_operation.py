a6542b72ff38ad503d72e6512f429f201fd78cde
Davuluri Harsha <davharsh@publicisgroupe.net>
2025-09-20 22:59:50 +0530

    shuffle concept refactoring

--- z_final/7.shuffle_operation.py
+++ z_final/7.shuffle_operation.py
@@ -1,30 +1,31 @@
 '''
 What is a Shuffle Operation in Spark?
 
-A shuffle operation in Spark is a mechanism for redistributing data across partitions and cluster
-nodes. During a shuffle, data is reorganized so that records with same key are moved to same
-partition.
+A shuffle operation in Spark is the process of redistributing data across partitions and cluster 
+nodes by moving records, so that those with the same key end up in the same partition.
 
-When Shuffle occur
-Shuffle operations are triggered by TRANSFORMATION that require data from multiple partitions to
-be combined or reorganized;
 
-* groupByKey, reduceByKey, aggregateByKey
-* join 
-* distinct
-* repartition and coalesce
-* sortBy and orderBy
+When Shuffle occurs?
+Shuffle occurs when data needs to be grouped, aggregated or joined based on a key.
+i.e WIDE TRANSFORMATIONS like 
+                        groupBy,
+                        joins, etc., 
+
+
+Optimization Strategies to avoid shuffles
+    - Leverage broadcast joins when one table is of small size
+    - Do repartition and coalesce wisely
+    - Prefer reduceByKey over groupByKey
+    - Apply bucketing on both tables using the same column before performing a join
+
+    
 
 Why Shuffles are expensive?
 Shuffles are among the most costly operations in spark bcz
 
 * Moving large amounts of data across cluster nodes
 * Writing intermediate shuffle files and reading them back
-* serialization/ Deserialization 
+* serialization/ Deserialization
 
 
-Optimization Strategies
-* Leverage broadcast joins for small datasets
-* use appropriate number of partitions through repartition and coalsece
-* Used reduceByKey instead of groupByKey
 '''
