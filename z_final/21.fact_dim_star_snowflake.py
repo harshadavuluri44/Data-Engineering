b875f4c897887dce3385dcbd70cf46987383d443
Davuluri Harsha <davuluri.harsha@epsilon.com>
2025-11-09 11:48:02 +0530

    restructuring

--- z_final/21.fact_dim_star_snowflake.py
+++ z_final/21.fact_dim_star_snowflake.py
@@ -55,5 +55,17 @@ This may cause repeated values but simplifies queries
 Advantages: faster querying (fewer joins)
 Disadvantages: redundant data (more storage)
 
+-----------------------------------------------------------------------------------------------
+
+When to Use Each (star vs snowflake)
+
+Star schema:
+    When query performance and simplicity(simple structure) are priorities
+    Best suited for OLAP systems when denormalization speeds up aggregations and joins
+
+Snowflake Schema:
+    When storage optimization or data consistency is more important
+    Used when dimensions are large(many columns or many rows) or have hierarchical relationships
+
 
 """
