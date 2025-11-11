ba379ad7762c31bb94ec9286bc0d822e7b436f5a
Davuluri Harsha <davuluri.harsha@epsilon.com>
2025-11-11 14:32:46 +0530

    enhance 16

--- z_final/16.repartition_coalesce.py
+++ z_final/16.repartition_coalesce.py
@@ -5,13 +5,15 @@ Spark.
 --------------------------------------------------------------------------------
 DIFFERENCES:
 
-1. repartition():
+1. df_r = df.repartition(100)
+
         Can increase or decrease the number of partitions.
         Always triggers a full shuffle of data to evenly redistribute it across partitions.
         Ensures partitions are balanced/even in size.
         More expensive in terms of computation.
 
-2. coalesce():
+2. df_c = df.coalesce(10)
+
         Can only decrease the number of partitions.
         Avoids a full shuffle by merging existing partitions together.
         May cause uneven/skewed partitions.
