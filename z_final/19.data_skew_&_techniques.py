2614120eadc58a9de65cfa302d433e00dc571e7f
Davuluri Harsha <davuluri.harsha@epsilon.com>
2025-11-11 14:13:25 +0530

    enhanced data skew

--- z_final/19.data_skew_&_techniques.py
+++ z_final/19.data_skew_&_techniques.py
@@ -5,8 +5,7 @@ What is Data Skew?
 Data Skew means when data is unevenly distributed across partitions in a cluster nodes of distributed
 computing system like spark.
 
-i.e Some partitions have huge amount of data (hot partitions)
-    Others have very little data
+i.e Some partitions have huge amount of data (hot partitions) and others have little data
 
 
 Data Skew causes 
@@ -21,6 +20,19 @@ Example :
 If 90% of rows have country = 'US' and other countries make up rest, then:
     Partition holding US data will be huge
     That executor will take much longer time to finish spark job
+------------------------------------------------------------------------------------------------
+
+How do you debug from spark UI to detect/confirm data skew ?
+
+
+1. Check the Jobs tab and note total duration and number of stages
+2. Identify which stage took longest time
+3. Click on the slow stage(from step 2) and open the Tasks
+4. Check tasks time and input size in tasks inside slow stage
+        If tasks are taking 10X longer time and
+        input size per task is more compared to other
+
+        Then we can confirm the skew.
 
 ------------------------------------------------------------------------------------------------
 HANDLING DATA SKEW
